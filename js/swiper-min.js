190aed2a8b5b6b0814a1c206ac765e940df53165
zzuo123 <33331699+zzuo123@users.noreply.github.com>
2021-02-01 22:53:09 -0500

    👌 IMPROVE: minified css and js

---
+++ js/swiper-min.js
@@ -0,0 +1,1 @@
+import Swiper from "https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js"; var swiper = new Swiper(".swiper-container", { slidesPerView: 1, loop: !0, simulateTouch: !0, keyboard: { enabled: !0 }, pagination: { el: ".swiper-pagination", clickable: !0 }, navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" } });
